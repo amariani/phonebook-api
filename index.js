5156c9c527bbf56382c3311cabf7a19188bc96c6
Ariel Mariani <ariel.mariani@noteworth.com>
2021-07-24 12:58:48 +0200

    Add env PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 morgan.token("body", (req, res) => JSON.stringify(req.body));
 morgan.format(

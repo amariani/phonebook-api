86f5f0c558e2f087471c69f201dc3d9870cd53b7
Ariel Mariani <ariel.mariani@noteworth.com>
2021-07-26 17:12:00 +0200

    Completed exercises 3.13 and 3.14

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const Contact = require("./models/contact");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 morgan.token("body", (req, res) => JSON.stringify(req.body));
 morgan.format(
@@ -15,33 +17,7 @@ app.use(express.static("build"));
 app.use(express.json());
 app.use(morgan("tinyWithBody"));
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1,
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3,
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4,
-  },
-];
-
 // Utilities
-const generateRandomId = (min, max) =>
-  Math.floor(Math.random() * (max - min) + min);
-
 const getRequiredFieldMessage = (fieldName) =>
   `Field ${fieldName} is required.`;
 
@@ -59,18 +35,25 @@ app.get("/info", (req, res) => {
 
 // Get list of persons
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Contact.find({}).then((personsResponse) => {
+    res.json(personsResponse);
+  });
 });
 
 // Get specific person if exists
 app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const personFound = persons.find((p) => p.id === id);
-  if (personFound) {
-    res.json(personFound);
-  } else {
-    res.status(404).end();
-  }
+  Contact.findById(req.params.id)
+    .then((contactFound) => {
+      if (contactFound) {
+        res.json(contactFound);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      response.status(400).send({ error: "Malformatted id" });
+    });
 });
 
 // Remove person
@@ -101,15 +84,14 @@ app.post("/api/persons", (req, res) => {
     });
   }
 
-  const newPerson = {
+  const contact = new Contact({
     name: body.name,
     number: body.number,
-    id: generateRandomId(1, 10000000),
-  };
+  });
 
-  persons = persons.concat(newPerson);
-
-  res.json(newPerson);
+  contact.save().then((newContact) => {
+    res.json(newContact);
+  });
 });
 
 app.listen(PORT);
---
+++ models/contact.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const url = process.env.MONGODB_URI;
+console.log("Initializing connection to", url);
+
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then((result) => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB:", error.message);
+  });
+
+const contactSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+// MongoDB Response Transformer
+contactSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Contact", contactSchema);
